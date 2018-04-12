1b8840f769abe3ca6c6676b1463b2bce7495ae6a
Anton <a.silischev@yandex.ru>
2018-04-12 22:30:54 +0300

    set docker + di

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,12 +1,21 @@
 <?php
 
+use function DI\object;
+use Otus\Core\RequestBuilder;
+use Otus\Interfaces\RequestBuilderInterface;
+
 $root = dirname(__DIR__);
 
 require "{$root}/vendor/autoload.php";
 
 $builder = new \DI\ContainerBuilder();
 
-// TODO: addDefinitions here
-//$builder->addDefinitions();
+$builder->addDefinitions([
+    RequestBuilderInterface::class => object(RequestBuilder::class),
+
+    /*'RequestBuilderInterface' => function() {
+        return new RequestBuilder();
+    }*/
+]);
 
 $container = $builder->build();
--- index.php
+++ index.php
@@ -3,12 +3,9 @@
 require_once(__DIR__ . '/bootstrap/autoload.php');
 require_once(__DIR__ . '/src/functions.php');
 
-pr($container, 1);
-
 $requestBuilder = $container->get(Otus\Interfaces\RequestBuilderInterface::class);
-pr($requestBuilder);
 
-/*$request = $requestBuilder->getRequest($_GET, $_POST);
+$request = $requestBuilder->getRequest($_GET, $_POST);
 
 $controllerFactory = $container->get(Otus\Interfaces\ControllerFactoryInterface::class);
 
@@ -16,4 +13,4 @@ $controller = $controllerFactory->getController($request);
 
 $response = $controller->execute($request);
 
-echo $response->getResponse();*/
+echo $response->getResponse();
